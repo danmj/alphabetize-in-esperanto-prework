36bdfbf6ea8bccedc296c19cc571086ca29b4dcf
danmj <daniel.jung@stern.nyu.edu>
2018-08-19 16:28:42 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,14 @@
 def alphabetize(arr)
   # code here
-  
+  result_array = []
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet_array = esperanto_alphabet.split("")
+  alphabet_array.each do |letter|
+  arr.each do |phrase|
+    if phrase[0] == letter
+      result_array << phrase
+    end
+  end
 end
+result_array
+end  
